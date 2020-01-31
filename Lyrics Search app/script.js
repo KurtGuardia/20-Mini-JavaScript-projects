32a464184b4a70f8edc893ac4d1b431e36b86c54
Kurt Guardia <kurtguardia@gmail.com>
2020-01-31 13:32:25 +0100

    Implemented the Next and Prev button functionality

--- Lyrics Search app/script.js
+++ Lyrics Search app/script.js
@@ -11,6 +11,7 @@ async function serchSongs(term) {
   //   fetch(`${apiURL}/suggest/${term}`)
   //     .then((res) => res.json())
   //     .then((data) => console.log(data));
+  // THE SAME WITH ASYNC AWAIT:
 
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
@@ -19,7 +20,65 @@ async function serchSongs(term) {
 }
 
 //Show song an artists in DOM
-function showData(data) {}
+function showData(data) {
+  //   let output = "";
+
+  //   data.data.forEach((song) => {
+  //     output = `
+  //     <li>
+  //         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //         <button class="btn" data-artists="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+  //     `;
+  //   });
+
+  //   result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  //   `;
+  // THE SAME BUT MORE CLEAN:
+
+  result.innerHTML = `
+    <ul class="songs">
+        ${data.data
+          .map(
+            (song) => `
+        <li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artists="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+        `
+          )
+          .join("")}
+    </ul>
+    `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+        ${
+          data.prev
+            ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+            : ``
+        }
+        ${
+          data.next
+            ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+            : ``
+        }
+        `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
 
 //Event listeners
 form.addEventListener("submit", (e) => {
