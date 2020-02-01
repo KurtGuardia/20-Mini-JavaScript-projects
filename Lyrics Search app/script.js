6a9bf04d02b0bd6d949a2dea94045631ce8578b4
Kurt Guardia <kurtguardia@gmail.com>
2020-02-01 13:53:27 +0100

    Show lyrics for selected song implemented. Project Done

--- Lyrics Search app/script.js
+++ Lyrics Search app/script.js
@@ -80,6 +80,19 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+//Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `<h2><strong>${artist}</strong> ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = "";
+}
+
 //Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -92,3 +105,17 @@ form.addEventListener("submit", (e) => {
     serchSongs(searchTerm);
   }
 });
+
+//Get lyrics button click
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artists");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
+  }
+});
+
+//MIO
