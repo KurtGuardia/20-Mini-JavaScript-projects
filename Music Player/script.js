1187611a5e430d78d3ba4d720d044de4113274f2
Kurt Guardia <kurtguardia@gmail.com>
2020-02-25 13:27:37 +0100

    Set the song to a different time, and next song when enede. Project Done

--- Music Player/script.js
+++ Music Player/script.js
@@ -11,7 +11,11 @@ const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
 //Song titles
-const songs = ['nothing else matters', 'whiskey in the jar', 'master of puppets']
+const songs = [
+  "nothing else matters",
+  "whiskey in the jar",
+  "master of puppets",
+];
 
 //Keep track of songs
 let songIndex = 0;
@@ -21,75 +25,87 @@ loadSong(songs[songIndex]);
 
 //Update song details
 function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `img/${song}.jpg`;
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `img/${song}.jpg`;
 }
 
 //Play song
 function playSong() {
-    musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-pause');
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
 
-    audio.play();
+  audio.play();
 }
 
 //Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
 
-    audio.pause();
+  audio.pause();
 }
 
 //previous song
 function prevSong() {
-    songIndex--;
-    if(songIndex < 0){
-        songIndex = songs.length - 1;
-    }
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
 
-    loadSong(songs[songIndex]);
+  loadSong(songs[songIndex]);
 
-    playSong();
+  playSong();
 }
 
 //next song
 function nextSong() {
-    songIndex++;
-    if(songIndex > songs.length - 1){
-        songIndex = 0;
-    }
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
 
-    loadSong(songs[songIndex]);
+  loadSong(songs[songIndex]);
 
-    playSong();
+  playSong();
 }
 
 //Update progress bar
-function updateProgress(e){
-    const {duration, currentTime} = e.srcElement;
-    console.log(duration, currentTime)
-    const progressPercent = (currentTime / duration) *100;
-    progress.style.width = `${progressPercent}%`;
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  console.log(duration, currentTime);
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
-//Event listeres
-playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
+//set progress bar
+function setProgress(e) {
+  const width = this.clientWisdth;
+  const clickX = e.offSetX;
+  const duration = audio.duration;
 
-    if(isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//Event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
 
 //Change song
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 //Time/song update
-audio.addEventListener('timeupdate', updateProgress);
-//
+audio.addEventListener("timeupdate", updateProgress);
+//Clickon progress bar
+progressContainer.addEventListener("click", setProgress);
+//Songs ends
+audio.addEventListener('ended', nextSong);
