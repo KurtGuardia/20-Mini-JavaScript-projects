aca150c89e221dc82e63858f2625f797d087fc4b
Kurt Guardia <kurtguardia@gmail.com>
2020-02-27 12:44:49 +0100

    added the filter functionality. Project Done

--- Infinite scroll posts/script.js
+++ Infinite scroll posts/script.js
@@ -7,7 +7,7 @@ let limit = 5;
 let page = 1;
 
 //fetch response
-async function gePost() {
+async function getPost() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page${page}`
   );
@@ -19,9 +19,9 @@ async function gePost() {
 
 //Show post in DOM
 async function showPost() {
-  const post = await gePost();
+  const post = await getPost();
 
-  post.forEach((post) => {
+  post.forEach((post) => { console.log(post.id)
     const postEl = document.createElement("div");
     postEl.classList.add("post");
     postEl.innerHTML = `<div class="number">${post.id}</div>
@@ -42,10 +42,27 @@ function showLoading() {
   setTimeout(() => {
     loading.classList.remove("show");
     setTimeout(() => {
-        page++;
-        showPost();
+      page++;
+      showPost();
     }, 300);
   }, 1000);
+}
+
+//Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+      const title = post.querySelector('.post-title').innerText.toUpperCase();;
+      const body = post.querySelector('.post-body').innerText.toUpperCase();;
+
+      if(title.indexOf(term) > -1 || body.indexOf(term) > -1){
+        post.style.display = 'flex';
+      } else {
+          post.style.display = 'none';
+      }
+  });
 }
 
 //Show initial posts
@@ -54,8 +71,10 @@ showPost();
 //Scroll event
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  console.log(scrollTop, scrollHeight, clientHeight);
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
+
+//filter
+filter.addEventListener("input", filterPosts);
