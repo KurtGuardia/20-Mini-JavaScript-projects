d3c244607020428a2ddea4dd668ae61f4fb3da42
Kurt Guardia <kurtguardia@gmail.com>
2020-03-19 18:45:19 +0100

    Fixed one bug

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -4,9 +4,10 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-let ticketPrice = +movieSelect.value;
 
 populateUI();
+
+let ticketPrice = movieSelect.value;
 
 //save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
@@ -17,9 +18,13 @@ function setMovieData(movieIndex, moviePrice) {
 //Update Total and Count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
+
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
@@ -27,17 +32,17 @@ function updateSelectedCount() {
 //Get data from local storage and populate UI
 function populateUI() {
   const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
-  if(selectedSeats !== null && selectedSeats.length > 0) {
-      seats.forEach((seat, index) => {
-          if(selectedSeats.indexOf(index) > -1){
-              seat.classList.add('selected');
-          }
-      })
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
   }
 
-  const selectedMovieindex = localStorage.getItem('selectedMovieIndex');
-  if(selectedMovieindex !== null) {
-      movieSelect.selectedIndex = selectedMovieItem;
+  let selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
   }
 }
 
