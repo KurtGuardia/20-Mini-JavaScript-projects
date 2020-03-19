50883ee3eb65897544060d1bd4daf646904854b3
Kurt Guardia <kurtguardia@gmail.com>
2020-03-19 18:00:56 +0100

    implemented local storage UI population functionality

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,21 +6,34 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+populateUI();
 
 //save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
-    localStorage.setItem('selectedMovieIndex', movieIndex);
-    localStorage.setItem('selectedMoviePrice', moviePrice);
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
 }
 
 //Update Total and Count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+          if(selectedSeats.indexOf(index) > -1){
+              seat.classList.add('selected');
+          }
+      })
+  }
 }
 
 //Event listener movie select
