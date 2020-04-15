f78dbf4d6177c591fc273b5164abd7a7265348e9
Kurt Guardia <kurtguardia@gmail.com>
2020-04-15 11:23:08 +0200

    worked with sort(), richest sorting implemented

--- DOM array methods/script.js
+++ DOM array methods/script.js
@@ -35,6 +35,12 @@ function doubleMoney() {
     updateDOM();
 }
 
+function sortByRichest(){
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+};
+
 //Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -65,3 +71,4 @@ function formatMoney(number) {
 //Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
