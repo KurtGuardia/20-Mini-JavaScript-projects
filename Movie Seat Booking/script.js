fa37b647bf80aadff753acfd1e178b778c6fdb3f
Kurt Guardia <kurtguardia@gmail.com>
2020-04-20 17:51:48 +0200

    added local storage functionality

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -1,35 +1,43 @@
 //Select DOM elements
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-const movieSelect = document.getElementById('movie');
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
+
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update Total and Count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected')
-    const seatsIndex = [...selectedSeats].map((seat) =>
-        [...seats].indexOf(seat) )    
-        console.log(seatsIndex)
-    const selectedSeatsCount = selectedSeats.length;
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-
 //Event listener movie select
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    updateSelectedCount();
-})
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+  setMovieData();
+});
 
 //Event listener seat click event
-container.addEventListener('click', (e) => {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected')
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
 
-        updateSelectedCount();
-    }
-})
-
+    updateSelectedCount();
+  }
+});
