4c66fb74ceb5f4203f8ffeefc3b04c38cefc88dd
Kurt Guardia <kurtguardia@gmail.com>
2020-07-19 10:19:27 +0200

    added the spinner and background year. Project Done

--- New Year Count Down/script.js
+++ New Year Count Down/script.js
@@ -4,11 +4,17 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+//Set background year
+year.innerText = currentYear + 1;
+
+//Update countdown time
 function updateCountDown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -18,10 +24,18 @@ function updateCountDown() {
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
+  //Add values to DOM
   days.innerHTML = d;
-  hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m < 10 ? '0' + m : m;
-  seconds.innerHTML = s < 10 ? '0' + s : s;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
+//show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+//Run every second
 setInterval(updateCountDown, 1000);
