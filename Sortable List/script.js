29db145184b30a423505bd57a1b5e9e44637d2af
Kurt Guardia <kurtguardia@gmail.com>
2021-03-03 15:54:18 +0100

    added Drag and Drop functionality

--- Sortable List/script.js
+++ Sortable List/script.js
@@ -34,11 +34,11 @@ function createList() {
 
       listItem.innerHTML = `
         <span class="number">${index + 1}</span>
-        <div class='draggable' draggable='true>
-            <p clas='person-name>${person}</p>
-            <i class='fas fa-grip-lines'></i>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
         </div>
-    `;
+      `;
 
       listItems.push(listItem);
 
@@ -48,15 +48,51 @@ function createList() {
   addEventListeners();
 }
 
+function dragEnter() {
+  //   console.log('Event: ', 'dragenter');
+  this.classList.add('over');
+}
+
+function dragOver(e) {
+  //   console.log('Event: ', 'dragover');
+  e.preventDefault();
+}
+
+function dragLeave() {
+  //   console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
+}
+
+function dragStart() {
+  //   console.log('Event: ', 'dragstart');
+  dragStartIndex = this.closest('li').getAttribute('data-index');
+}
+
+function dragDrop() {
+  //   console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
 function addEventListeners() {
   const draggables = document.querySelectorAll('.draggable');
-  const dragListItems = document.querySelectorall('.draggable-list li');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
 
   draggables.forEach((dragabble) => {
     dragabble.addEventListener('dragstart', dragStart);
   });
 
-  dragListItems.forEach((items) => {
+  dragListItems.forEach((item) => {
     item.addEventListener('dragover', dragOver);
     item.addEventListener('drop', dragDrop);
     item.addEventListener('dragenter', dragEnter);
