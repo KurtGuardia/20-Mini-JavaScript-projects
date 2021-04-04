2508a0a7ca3dee01877ce050a2b18d9544a3a4ca
Kurt Guardia <kurtguardia@gmail.com>
2021-04-04 09:19:51 +0200

    done check functions and write to DOM

--- Speak Number Guessing Game/script.js
+++ Speak Number Guessing Game/script.js
@@ -1,4 +1,4 @@
-const mesgEl = document.getElementById('msg');
+const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
 
@@ -16,8 +16,43 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  //   writeMessage(msg)
-  //   checkNumber(msg)
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+//Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>`;
+}
+
+//check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  //check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  //check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  //check number
+  if (num == randomNum) {
+    document.body.innerHtml = `<h2>Congrats! yopu have guessed the number! <br><br>
+      It was ${num} </h2>
+      <button class="play-again" id="play-again"> Play Again</button>`;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>Go Lower</div>';
+  } else {
+    msgEl.innerHTML += '<div>Go Lower</div>';
+  }
 }
 
 //Generate random number
