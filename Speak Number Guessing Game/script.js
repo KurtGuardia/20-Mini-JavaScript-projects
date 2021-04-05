49f88b7a299fc3aa625c930ebf2aca2fdcdd5acd
Kurt Guardia <kurtguardia@gmail.com>
2021-04-05 09:25:08 +0200

    Implemented keep playing and reload functionality

--- Speak Number Guessing Game/script.js
+++ Speak Number Guessing Game/script.js
@@ -2,17 +2,17 @@ const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
 
-console.log('Numbert: ' + randomNum);
+console.log('Number:', randomNum);
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
 let recognition = new window.SpeechRecognition();
 
-//start recognition and game
+// Start recognition and game
 recognition.start();
 
-//Capture user speak
+// Capture user speak
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
@@ -20,45 +20,57 @@ function onSpeak(e) {
   checkNumber(msg);
 }
 
-//Write what user speaks
+// Write what user speaks
 function writeMessage(msg) {
   msgEl.innerHTML = `
     <div>You said: </div>
-    <span class="box">${msg}</span>`;
+    <span class="box">${msg}</span>
+  `;
 }
 
-//check msg against number
+// Check msg against number
 function checkNumber(msg) {
   const num = +msg;
 
-  //check if valid number
+  // Check if valid number
   if (Number.isNaN(num)) {
     msgEl.innerHTML += '<div>That is not a valid number</div>';
     return;
   }
 
-  //check in range
+  // Check in range
   if (num > 100 || num < 1) {
     msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
     return;
   }
 
-  //check number
-  if (num == randomNum) {
-    document.body.innerHtml = `<h2>Congrats! yopu have guessed the number! <br><br>
-      It was ${num} </h2>
-      <button class="play-again" id="play-again"> Play Again</button>`;
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
   } else if (num > randomNum) {
-    msgEl.innerHTML += '<div>Go Lower</div>';
+    msgEl.innerHTML += '<div>GO LOWER</div>';
   } else {
-    msgEl.innerHTML += '<div>Go Lower</div>';
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
   }
 }
 
-//Generate random number
+// Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
